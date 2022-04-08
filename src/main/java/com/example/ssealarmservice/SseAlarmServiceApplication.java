8d0a0591af6d31658afe10f52a5fed2347aa03a0
kingchan223 <kokiyo97@naver.com>
2022-04-08 21:50:49 +0900

    first

---
+++ src/main/java/com/example/ssealarmservice/SseAlarmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssealarmservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseAlarmServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseAlarmServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ssealarmservice/SseAlarmServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssealarmservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseAlarmServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
